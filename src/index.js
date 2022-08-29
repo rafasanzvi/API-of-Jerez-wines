577ba5ce4b258b75b7e8edda2ffe802837f047a7
rafasanzvi <rafasv41@gmail.com>
2022-08-29 23:30:14 +0200

    server connected to the data base

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require("dotenv").config() //to customise environment variables in .env
 
 const app = express();
 const port = process.env.PORT || 9000 //Server in port 9000
+app.listen(port, () => console.log('server listening on port', port)) //Server listening on port = 9000 
 
 //Routes
 app.get('/', (req, res) => {
-    res.send("<h1>No arriesgues mamahuevo!!!!<h1>")
+    res.send("<h1>Hiiiiiiiii!!!!<h1>")
 })
 
-app.listen(port, () => console.log('server listening on port', port)) //Server listening on port = 9000 
+// Mongodb connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB Compass"))
+    .catch((error) => console.error(error))
+
+
