28f7d0da20a6392aaa32969217cbbc2b560ff60d
rafasanzvi <rafasv41@gmail.com>
2022-08-29 20:37:43 +0200

    Main route created and mongoose and nodemon installed

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 9000 //Server in port 9000
 
-app.listen(port, () => console.log('server listening on port', port)) //Server listening on port = 9000
+//Routes
+app.get('/', (req, res) => {
+    res.send("<h1>No arriesgues mamahuevo!!!!<h1>")
+})
+
+app.listen(port, () => console.log('server listening on port', port)) //Server listening on port = 9000 
